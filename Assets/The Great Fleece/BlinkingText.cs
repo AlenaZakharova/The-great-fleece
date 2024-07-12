e840a8419070416db943b40752cbc523a3c5ff9b
Alena Zakharova <38889670+alenazakharova@users.noreply.github.com>
2024-07-13 02:09:34 +0300

    Set up shots according to voice track. Createt BlinkingText.cs srcipt and applied it to skip intro hint

---
+++ Assets/The Great Fleece/BlinkingText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace The_Great_Fleece
+{
+    
+    public class BlinkingText : MonoBehaviour
+    {
+        [SerializeField] private Text _text;
+        private Graphic _graphic;
+        void Start()
+        {
+            _graphic = _text.GetComponent<Graphic>();
+        }
+
+        private void Update()
+        {
+            _graphic.color = Color.Lerp(Color.white, new Color(1,1,1,0), Mathf.PingPong(Time.time, 1));
+        }
+    }
+}
